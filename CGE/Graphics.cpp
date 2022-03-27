f2111daf641df62caf79017cf000a61f7834c3b1
Len <glizzyzbro@hotmail.com>
2022-03-27 21:48:50 +1100

    [SYNC]

--- CGE/Graphics.cpp
+++
@@ -1,6 +0,0 @@
-#include "Graphics.hpp"
-
-bool Graphics::InterpolateColours   = false;
-bool Graphics::InterpolateNormals   = false;
-bool Graphics::InterpolatePositions = false;
-bool Graphics::InterpolateTexels    = false;
