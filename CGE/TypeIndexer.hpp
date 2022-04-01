07698d894b369732c586791fe65afb6da79131ee
Work PC <glizzyzbro@hotmail.com>
2022-04-01 16:32:19 +1100

    awdaw

--- CGE/TypeIndexer.hpp
+++ CGE/TypeIndexer.hpp
@@ -19,3 +19,40 @@ public:
 		return Index;
 	}
 };
+
+typedef TypeIndexer< "ResourceTypeIndexer"_H > ResourceTypeIndexer;
+#include <vector>
+void foo()
+{
+	size_t intIndex = ResourceTypeIndexer::GetTypeIndex< int >();
+	size_t intIndex1 = ResourceTypeIndexer::GetTypeIndex< int >();
+	size_t floatIndex = ResourceTypeIndexer::GetTypeIndex< float >();
+}
+
+std::vector< void* > resources;
+
+template < typename T, typename... Args >
+void CreateResource( Args&&... args )
+{
+	int index = ResourceTypeIndexer::GetTypeIndex< T >();
+
+	if ( index >= resources.size() )
+	{
+		resources.resize( index + 1 );
+	}
+
+	resources[ index ] = new T( std::forward( args )... );
+}
+
+template < typename T >
+T* GetResource()
+{
+	int index = ResourceTypeIndexer::GetTypeIndex< T >();
+
+	if ( index >= resources.size() )
+	{
+		return nullptr;
+	}
+
+	return reinterpret_cast< T* >( resources[ index ] );
+}
